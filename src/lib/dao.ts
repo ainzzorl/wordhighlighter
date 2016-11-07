d2c5cf4506c209f13302bca769823a2ac8b7d979
ainzzorl <ainzzorl@gmail.com>
2016-11-07 09:11:06 -0800

    Update src build path

--- src/lib/dao.ts
+++ src/lib/dao.ts
@@ -1,4 +1,4 @@
-///<reference path="../node_modules/@types/chrome/index.d.ts" />
+///<reference path="../../node_modules/@types/chrome/index.d.ts" />
 
 // TODO: use synced storage
 class DAO {
---
+++ src/scripts/background-script.ts
@@ -0,0 +1,3 @@
+///<reference path="../lib/dao.ts" />
+
+new DAO().init();
