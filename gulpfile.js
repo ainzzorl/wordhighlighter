06e5dae1b86302017d1f952fb0ea7c0b04c1e972
ainzzorl <ainzzorl@gmail.com>
2016-11-06 17:29:42 -0800

    Add an always passing jasmine test

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var tsProjectSrc = ts.createProject('tsconfig-src.json');
+var tsProjectSpec = ts.createProject('tsconfig-spec.json');
+var jasmine = require('gulp-jasmine');
 
 gulp.task('copy-static-content', function () {
     return gulp.src(['manifest.json', 'wordhighlighter.css'])
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('compile', function () {
-    return tsProject.src()
-        .pipe(tsProject())
+gulp.task('compile-src', function () {
+    return tsProjectSrc.src()
+        .pipe(tsProjectSrc())
         .js.pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['copy-static-content', 'compile']);
+gulp.task('compile-spec', function () {
+    return tsProjectSpec.src()
+            .pipe(tsProjectSpec())
+            .js.pipe(gulp.dest('build/spec'));
+});
+
+gulp.task('spec', ['compile-src', 'compile-spec'], function() {
+    return gulp.src('build/spec/*Spec.js').pipe(jasmine());
+});
+
+
+gulp.task('default', ['copy-static-content', 'compile-src', 'compile-spec', 'spec']);
---
+++ spec/testSpec.ts
@@ -0,0 +1,7 @@
+///<reference path="../node_modules/@types/jasmine/index.d.ts" />
+
+describe('A suite', function() {
+  it('always succeeds', function() {
+    expect(true).toBe(true);
+  });
+});
