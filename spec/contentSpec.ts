c256b1293334bf0f011ca1689ecadc8a231a22b1
ainzzorl <ainzzorl@gmail.com>
2016-11-19 19:30:45 -0800

    Add no match test for Content

--- spec/contentSpec.ts
+++ spec/contentSpec.ts
@@ -3,33 +3,52 @@
 
 describe('content', function() {
     let textNodeHandler: TextNodeHandler;
+    let rootElement;
+    let content;
 
-    describe('start', function() {
-        let rootElement;
+    beforeEach(function() {
+        textNodeHandler = new TextNodeHandler(null);
+        rootElement = document.createElement('div');
+        content = new Content(textNodeHandler);
 
-        beforeEach(function() {
-            let textNodeHandler = new TextNodeHandler(null);
-            textNodeHandler.injectMarkup = function(node: Node): Array<HTMLElement> {
-                if (node.textContent === 'Replaced with 2') {
-                    return [
-                        createSpan('span1'),
-                        createSpan('span2')
-                    ];
-                }
+        textNodeHandler.injectMarkup = function(node: Node): Array<HTMLElement> {
+            if (node.textContent === 'Replaced with 2') {
+                return [
+                    createSpan('span1'),
+                    createSpan('span2')
+                ];
+            }
+            if (node.textContent === 'Does not match') {
                 return null;
             }
-            rootElement = document.createElement('div');
-            rootElement.innerHTML = '<child>Replaced with 2</child>';
-            document.body.appendChild(rootElement);
+            return [];
+        }
+    });
 
-            let content = new Content(textNodeHandler);
-            content.injectMarkup(rootElement);
-        });
+    describe('one text node', function() {
+        describe('match', function() {
+            beforeEach(function() {
+                rootElement.innerHTML = '<child>Replaced with 2</child>';
+                content.injectMarkup(rootElement);
+            });
 
-        it('marks up the node', function() {
-            expect(rootElement.innerHTML).toEqual(
-                '<child><span>span1</span><span>span2</span></child>');
-        });
+            it('marks up the text', function() {
+                expect(rootElement.innerHTML).toEqual(
+                    '<child><span>span1</span><span>span2</span></child>');
+            });
+        })
+
+        describe('no match', function() {
+            beforeEach(function() {
+                rootElement.innerHTML = '<child>Does not match</child>';
+                content.injectMarkup(rootElement);
+            });
+
+            it('does not do anything', function() {
+                expect(rootElement.innerHTML).toEqual(
+                    '<child>Does not match</child>');
+            });
+        })
     });
 
     function createSpan(value) {
