f6dd13949711010e5f49474af9a36a113741e3a8
ainzzorl <ainzzorl@gmail.com>
2016-12-03 09:42:22 -0800

    Externalize import modes

--- src/mainDialog/importController.ts
+++ src/mainDialog/importController.ts
@@ -1,9 +1,14 @@
 angular
 .module('mainDialog')
 .controller('importController', function($scope: any) {
+
+    $scope.MODE_KEEP = 'keep';
+    $scope.MODE_OVERWRITE = 'overwrite';
+    $scope.MODE_REPLACE = 'replace';
+
     $scope.importInput = {
         data: '',
-        mode: 'keep'
+        mode: $scope.MODE_KEEP
     };
 
     $scope.onImportClicked = function() {
