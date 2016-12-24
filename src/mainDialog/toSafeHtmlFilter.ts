44399726ddf2ecbe53417de5a73c383d548c599e
ainzzorl <ainzzorl@gmail.com>
2016-12-24 11:57:30 -0800

    Display multi-line descriptions in the table

---
+++ src/mainDialog/toSafeHtmlFilter.ts
@@ -0,0 +1,10 @@
+// TODO: unit test
+
+angular
+.module('mainDialog')
+.filter('toSafeHtml', ['$sce', function($sce: any) {
+    return function(text: string): string {
+        let result = text ? text.replace(/\n/g, '<br>') : '';
+        return $sce.trustAsHtml(result);
+    };
+}]);
