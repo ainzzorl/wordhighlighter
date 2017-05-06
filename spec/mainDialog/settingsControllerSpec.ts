c1bfd9973de9a8d9c2b606622361cb9ea2316218
ainzzorl <ainzzorl@gmail.com>
2017-05-06 12:25:26 -0700

    Separate out main dialog spec

--- spec/mainDialog/settingsControllerSpec.ts
+++ spec/mainDialog/settingsControllerSpec.ts
@@ -1,6 +1,6 @@
-///<reference path="../node_modules/@types/jasmine/index.d.ts" />
-///<reference path="../node_modules/@types/angular/index.d.ts" />
-///<reference path="../src/lib/common/settings.ts" />
+///<reference path="../../node_modules/@types/jasmine/index.d.ts" />
+///<reference path="../../node_modules/@types/angular/index.d.ts" />
+///<reference path="../../src/lib/common/settings.ts" />
 
 describe('settingsController', function() {
 
